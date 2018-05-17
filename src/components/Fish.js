491a23b460deb5177597ebbd82ec240cc8cdf407
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-05-18 01:14:06 +0500

    Initial commit

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <li className="single-fish">
+        fish
+      </li>
+    );
+  }
+}
+
+export default Fish;
