172d62713d2d3101efd7ca28f1c1769b763bdf50
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-06-05 01:39:38 +0500

    added login form and facebook auth provider

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -11,25 +14,35 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   };
 
+  authHandler = async (authData) => {
+    console.log(authData);
+  }
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebase.auth(firebaseApp).signInWithPopup(authProvider).then(this.authHandler);
+  };
+
   render() {
-    return (
-      <div className="inventory">
-        <h2>Inventory</h2>
-        {Object.keys(this.props.fishes).map(key => (
-          <EditFishForm
-            key={key}
-            index={key}
-            updateFish={this.props.updateFish}
-            deleteFish={this.props.deleteFish}
-            fish={this.props.fishes[key]}
-          />
-        ))}
-        <AddFishForm addFish={this.props.addFish} />
-        <button onClick={this.props.loadSampleFishes}>
-          Load Sample Fishes
-        </button>
-      </div>
-    );
+    return <Login authenticate={this.authenticate} />;
+    // return (
+    //   <div className="inventory">
+    //     <h2>Inventory</h2>
+    //     {Object.keys(this.props.fishes).map(key => (
+    //       <EditFishForm
+    //         key={key}
+    //         index={key}
+    //         updateFish={this.props.updateFish}
+    //         deleteFish={this.props.deleteFish}
+    //         fish={this.props.fishes[key]}
+    //       />
+    //     ))}
+    //     <AddFishForm addFish={this.props.addFish} />
+    //     <button onClick={this.props.loadSampleFishes}>
+    //       Load Sample Fishes
+    //     </button>
+    //   </div>
+    // );
   }
 }
 
